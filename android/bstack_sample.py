f9fc15357f9121e74063cccaa156d91d4f8c7a9d
Samiran Saha <samiran.s89@gmail.com>
2025-11-09 21:31:14 +0530

    cleanup for now

--- android/bstack_sample.py
+++ android/bstack_sample.py
@@ -11,19 +11,7 @@ from selenium.webdriver.common.by import By
 class TestSample:
 
     def test_example(self):
-        search_element = WebDriverWait(self.driver, 30).until(
-            EC.element_to_be_clickable(
-                (AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia"))
-            )
-
-        search_element.click()
-        search_input = WebDriverWait(self.driver, 30).until(
-            EC.element_to_be_clickable(
-                (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text"))
-            )
-        search_input.send_keys("BrowserStack")
-        time.sleep(5)
-        search_results = self.driver.find_elements(
-            AppiumBy.CLASS_NAME, "android.widget.TextView")
-        
-        assert len(search_results) > 0
+        page_source = self.driver.page_source
+        print(f"Page Source Length: {len(page_source)}")
+        # Assert that the page source length > 100
+        assert len(page_source) > 100, "Page source length is not greater than 100!"
