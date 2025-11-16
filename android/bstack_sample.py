653e40a8401960e43aadb2862fa22e9b3c9d4770
Samiran Saha <samiran.s89@gmail.com>
2025-11-16 20:14:42 +0530

    pytest appium cleanup changes

--- android/bstack_sample.py
+++ android/bstack_sample.py
@@ -1,9 +1,3 @@
-from browserstack.local import Local
-from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 import pytest
 from selenium.webdriver.common.by import By
 
