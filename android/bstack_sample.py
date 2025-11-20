2a48db4f2f5628a8354c7911cca27c590aa5ff51
Samiran Saha <samiran.s89@gmail.com>
2025-11-20 17:28:49 +0530

    bstack local + minor python changes

--- android/bstack_sample.py
+++ android/bstack_sample.py
@@ -4,8 +4,12 @@ from selenium.webdriver.common.by import By
 @pytest.mark.usefixtures('setWebdriver')
 class TestSample:
 
-    def test_example(self):
-        page_source = self.driver.page_source
+    def test_example(setWebdriver):
+        driver = setWebdriver
+        # Example: open app or check activity
+        print(driver.session_id)
+        page_source = driver.page_source
         print(f"Page Source Length: {len(page_source)}")
         # Assert that the page source length > 100
         assert len(page_source) > 100, "Page source length is not greater than 100!"
+        
